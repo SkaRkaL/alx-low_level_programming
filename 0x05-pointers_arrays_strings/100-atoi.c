df6012832423d22751d2a9e63e20933f8f41944e
SkaRkaL <karkalsaad@gmail.com>
2023-04-03 02:35:21 -0700

    done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,33 @@
  */
 int _atoi(char *s)
 {
-	int	i;
-	int	signe;
-	int	resultat;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	i = 0;
-	signe = 1;
-	resultat = 0;
-	while (((s[i] >= 9) && (s[i] <= 13)) || (s[i] == 32))
-		i++;
-	while (s[i] == '+' || s[i] == '-')
+	while (s[c])
 	{
-		if (s[i] == '-')
-			signe *= -1;
-		i++;
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (isi == 1)
+		{
+			break;
+		}
+
+		c++;
 	}
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		resultat = resultat * 10 + (s[i] - '0');
-		i++;
-	}
-	return (resultat * signe);
+
+	ni *= min;
+	return (ni);
 }
